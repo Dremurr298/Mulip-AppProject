892820b489ee7800005e0a390ed9cf1430d292c2
Dremurr298 <83453659+Dremurr298@users.noreply.github.com>
2023-05-02 13:34:57 +0700

    Push to github

---
+++ Login.js
@@ -0,0 +1,80 @@
+window.onload = function() {
+    if(document.readyState == "complete") {
+    	document.getElementById("checkmark-input").addEventListener("click", () => {
+    		let checking = document.querySelector(".checkmark-input").checked
+    		if(!checking) {
+    			return document.getElementById("passwordForm").type = "password"
+    		}
+    		return document.getElementById("passwordForm").type = "text"
+    	})
+
+    	document.getElementById("submitForm").addEventListener("click", () => {
+    		let email = document.getElementById("emailForm").value
+    		let password = document.getElementById("passwordForm").value
+
+    		document.getElementById('submitForm').innerHTML = `
+	    		<div style="
+					width: 25px;
+					height: 25px;
+					border-radius: 50%;
+					border: 7px solid #FFFFFF;
+					border-top: 7px solid #23272A;
+					animation: rotating 1s linear infinite;
+					margin: 0 auto;
+				"></div>
+    		`
+
+    		if(!email && !password || !email || !password) {
+    			document.getElementById("smalltext").innerText = "Tolong Isi Semua Param!"
+    			document.getElementById("submitForm").innerHTML = "Submit"
+    			return
+    		}
+
+    		document.getElementById("smalltext").innerText = "\u00a0"
+    		var em_regex = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/
+
+    		if(!em_regex.test(email)) {
+    			document.getElementById("smalltext").innerText = "Email Invalid!"
+    			document.getElementById("submitForm").innerHTML = "Submit"
+    			return
+    		}
+
+    		fetch(`http://127.0.0.1:3000/login?email=${email}&password=${password}`)
+    		.then(response => response.json())
+    		.then(data => {
+    			if(data['status'] == 404) {
+    				document.getElementById("submitForm").innerHTML = "Submit"
+    				document.getElementById("smalltext").innerText = "Email Tidak Ditemukan!"
+    				return
+    			}
+
+    			else if(data['status'] == 401) {
+    				document.getElementById("submitForm").innerHTML = "Submit"
+    				document.getElementById("smalltext").innerText = "Password Salah!"
+    				return
+    			}
+
+    			return document.getElementById("submitForm").innerHTML = "Submit"
+    		})
+    		.catch((err)=>{
+    			console.log(err)
+			})
+    	})
+
+    	document.getElementById('emailForm').addEventListener('keyup', () => {
+    		let email = document.getElementById("emailForm").value
+    		var em_regex = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/
+
+    		if(!em_regex.test(email)) {
+    			document.getElementById("EnvelopeSvgs").innerHTML = `
+    				<img src="Envelope_invalid.svg" style="width: 45px;height: 45px;">
+    			`
+    			return
+    		}
+
+    		document.getElementById("EnvelopeSvgs").innerHTML = `
+				<img src="Envelope_valid.svg" style="width: 45px;height: 45px;">
+    		`
+    	})
+    }
+}
---
+++ server.js
@@ -0,0 +1,137 @@
+var cors = require('cors')
+
+// EXPRESS
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname + '/'))
+app.use(cors({ origin: '*' }));
+
+app.use(function(req, res, next) {
+	res.setHeader('Content-Security-Policy', "default-src 'self' data: https://ssl.gstatic.com 'unsafe-eval' http://127.0.0.1:3000")
+	next();
+})
+
+// MONGODB
+const MongoClient = require("mongodb").MongoClient
+const url = process.env.MULIPDB
+
+// SOCKET IO
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+// MONGODB SIMPLEST FUNCTION
+async function cursor_insert(obj) {
+	return new Promise((resolve, reject) => {
+		MongoClient.connect(url, function(err, client) {
+			if (err) reject(err)
+
+			var cursor = client.db("MulipDB").collection("Data")
+			cursor.insertOne(obj, function(err, res) {
+		    	if (err) reject(err)
+		    	client.close()
+		    	return resolve(res)
+			})
+		})
+	})
+}
+
+async function cursor_find(obj_search, obj_id) {
+	return new Promise((resolve, reject) => {
+		MongoClient.connect(url, function(err, client) {
+			if (err) reject(err)
+
+			var cursor = client.db("MulipDB").collection("Data")
+			cursor.findOne({[obj_search]:obj_id}, function(err, res) {
+		    	if (err) throw reject(err)
+		    	client.close()
+		    	return resolve(res)
+			})
+		})
+	})
+}
+
+async function cursor_update(obj_id, obj) {
+	return new Promise((resolve, reject) => {
+		MongoClient.connect(url, function(err, client) {
+			if (err) reject(err)
+
+			var cursor = client.db("MulipDB").collection("Data")
+			cursor.updateOne({"ID":obj_id}, {$set: obj}, function(err, res) {
+				if (err) reject(err)
+				client.close()
+				return resolve(res)
+			})
+		})
+	})
+}
+
+function cursor_delete(obj_id) {
+	return new Promise((resolve, reject) => {
+		MongoClient.connect(url, function(err, client) {
+			if (err) reject(err)
+
+			var cursor = client.db("MulipDB").collection("Data")
+			cursor.deleteOne({"ID":obj_id}, function(err, res) {
+		    	if (err) throw err
+		    	client.close()
+		    	return resolve(res)
+			})
+		})
+	})
+}
+
+async function main() {
+	let get_data = await cursor_find("Email", "MYID_LEMAO")
+	if(!get_data) return console.log("DATA IS NOT EXIST")
+	return console.log(get_data)
+}
+
+// EXPRESS WEBSITE PATH..?
+
+app.get("/", async function(req,res) {
+    res.send({
+    	status: 200,
+    	message: "SERVER IS ONLINE"
+    })
+})
+
+app.get("/signup", async function(req, res) {
+    var Email = req.query.email
+    var Password = req.query.password
+})
+
+app.get("/login", async function(req, res) {	// http://<DOMAIN>/login?email=EMAIL@gmail.com&password=passwords
+    var Email = req.query.email
+    var Password = req.query.password
+
+    // [a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$ EMAIL REGEX
+    let get_data = await cursor_find("Email", `${Email}`)
+    if(get_data == null || get_data == " ") {
+    	return res.send({
+    		status: 404,
+    		message: "Data Not Found",
+    		data: null
+    	})
+    }
+
+    if(Password != get_data['Password']) {
+    	return res.send({
+    		status: 401,
+    		message: "Invalid credentials",
+    		data: null
+    	})
+    }
+
+    return res.send({
+    	status: 200,
+    	message: "Response is resolved",
+    	data: get_data
+    })
+})
+
+// START SERVER
+http.listen(port, () => {
+    console.log(`Socket.IO server running at http://localhost:${port}/`)
+})
