fa4f10f356f01221ec92b321f1e0ed24520c8920
Dremurr298 <83453659+Dremurr298@users.noreply.github.com>
2023-05-02 14:58:11 +0700

    02/05/2023

--- Login.js
+++ Login.js
@@ -12,21 +12,13 @@ window.onload = function() {
     		let email = document.getElementById("emailForm").value
     		let password = document.getElementById("passwordForm").value
 
-    		document.getElementById('submitForm').innerHTML = `
-	    		<div style="
-					width: 25px;
-					height: 25px;
-					border-radius: 50%;
-					border: 7px solid #FFFFFF;
-					border-top: 7px solid #23272A;
-					animation: rotating 1s linear infinite;
-					margin: 0 auto;
-				"></div>
-    		`
+    		document.getElementById("LdAnim").style.display = "block"
+    		document.getElementById("SBLB").style.display = "none"
 
     		if(!email && !password || !email || !password) {
     			document.getElementById("smalltext").innerText = "Tolong Isi Semua Param!"
-    			document.getElementById("submitForm").innerHTML = "Submit"
+    			document.getElementById("LdAnim").style.display = "none"
+    			document.getElementById("SBLB").style.display = "block"
     			return
     		}
 
@@ -35,26 +27,31 @@ window.onload = function() {
 
     		if(!em_regex.test(email)) {
     			document.getElementById("smalltext").innerText = "Email Invalid!"
-    			document.getElementById("submitForm").innerHTML = "Submit"
+    			document.getElementById("LdAnim").style.display = "none"
+    			document.getElementById("SBLB").style.display = "block"
     			return
     		}
 
-    		fetch(`http://127.0.0.1:3000/login?email=${email}&password=${password}`)
+    		fetch(`https://mulip-appproject-production.up.railway.app/login?email=${email}&password=${password}`)
     		.then(response => response.json())
     		.then(data => {
     			if(data['status'] == 404) {
-    				document.getElementById("submitForm").innerHTML = "Submit"
+    				document.getElementById("LdAnim").style.display = "none"
+    				document.getElementById("SBLB").style.display = "block"
     				document.getElementById("smalltext").innerText = "Email Tidak Ditemukan!"
     				return
     			}
 
     			else if(data['status'] == 401) {
-    				document.getElementById("submitForm").innerHTML = "Submit"
+    				document.getElementById("LdAnim").style.display = "none"
+    				document.getElementById("SBLB").style.display = "block"
     				document.getElementById("smalltext").innerText = "Password Salah!"
     				return
     			}
 
-    			return document.getElementById("submitForm").innerHTML = "Submit"
+    			document.getElementById("LdAnim").style.display = "none"
+    			document.getElementById("SBLB").style.display = "block"
+    			return
     		})
     		.catch((err)=>{
     			console.log(err)
